f80259306e18df369ef6148605913027e8b2a6e2
teiatan <92535482+teiatan@users.noreply.github.com>
2022-12-07 15:43:02 +0200

    promises position declaration

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,7 +17,7 @@ function formSubmit(e) {
 
   console.log(step);
 
-  for (position = 1; position <= amount; position += 1) {
+  for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
     .then(({ position, delay}) => {
       setTimeout(() => {
