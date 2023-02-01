ce01895cd3d8f6a22a9cd2ea17f7f8fe80285c03
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-01 13:59:26 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,8 +15,7 @@ function formSubmit(e) {
   const step = Number(refs.step.value);
   const amount = Number(refs.amount.value);
   refs.form.reset();
-  console.log(step);
-
+  
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
     .then(({ position, delay}) => {
